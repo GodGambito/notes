158cd2729a6ff182eef6ad0ce033f9ab918c7b07
Mervin <mfnol@roensa.cl>
2020-01-02 12:06:40 -0300

    Modif database

--- src/database.js
+++ src/database.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 mongoose.set('useFindAndModify', false);
 mongoose.connect('mongodb+srv://mervin:gambito3@cluster0-7w3jt.mongodb.net/test?retryWrites=true&w=majority', {
   useCreateIndex: true,
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
   .then(db => console.log('DB is connected'))
   .catch(err => console.error(err));
